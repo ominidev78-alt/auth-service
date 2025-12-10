03141180329cd31f07edbadd6c6c81163f9f643c
Edmilton Pansanato <edmilton.vinicius2@gmail.com>
2025-12-10 17:00:54 -0300

    ajustando start da aplicacao

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import { app } from './app.js'
-import { env } from './config/env.js'
-import { initDb } from './config/db.js'
+import { env } from './config/env.js';
 
 async function start() {
     try {
-        await initDb()
 
-        const PORT = process.env.PORT || 3001
+        const PORT = env.PORT || 3001
 
         app.listen(PORT, '0.0.0.0', () => {
             console.log(`auth-service rodando na porta ${PORT}`)
