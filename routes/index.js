e28cfa5472609fb4b51d41b53bfb5caf1a2ad94b
Edmilton Pansanato <edmilton.vinicius2@gmail.com>
2025-12-11 17:11:35 -0300

    adicionando rota adminMaintenanceRoutes

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ import userAuthRoutes from './auth.user.routes.js';
 import twoFactorAuthRoutes from './twoFactorAuth.routes.js';
 import internalRoutes from './internal.routes.js';
 import publicRoutes from './public.routes.js';
+import adminMaintenanceRoutes from './admin.maintenance.routes.js';
 
 const router = Router();
 
 router.use('/api', authRoutes);
 router.use('/api', userAuthRoutes);
 router.use('/api', twoFactorAuthRoutes);
+router.use('/api', adminMaintenanceRoutes);
 router.use('/api/internal', internalRoutes);
 router.use('/api/public', publicRoutes);
 
